044179939d6264a79fa64e339aeb61ef91560fac
Dante Schacht <dantesch10@gmail.com>
2025-07-01 20:48:47 -0400

    Se agrego la prueba con junit a Service

---
+++ src/test/java/com/Soporte/services/TicketSoporteServiceTest.java
@@ -0,0 +1,20 @@
+package com.Soporte.services;
+
+import org.junit.jupiter.api.Test;
+
+public class TicketSoporteServiceTest {
+    @Test
+    void testActualizarEstado() {
+
+    }
+
+    @Test
+    void testCrearTicket() {
+
+    }
+
+    @Test
+    void testListarTicketsPorCliente() {
+
+    }
+}
